5e559060fde0758d75245e3800ddc90462f30f5b
Relemore <vladod0112@gmail.com>
2021-02-15 18:53:54 +0300

    Practice 1

--- main.py
+++ main.py
@@ -1,16 +1,48 @@
-# This is a sample Python script.
+import math
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def first(x):
+    y = (x**5/78-21*x**3)**0.5 + 2*x**8 + 21*x**7 + (51*x**7 + x**3)/(x**8 + math.cos(x) - 3)
+    return y
+print("Задача 1.1. Реализовать функцию")
+print ("f(57) = {:.2e}".format(first(57)))
+print ("f(94) = {:.2e}".format(first(94)))
+print()
 
+def second(x):
+    if x < -8:
+        return x**5/59 - 34*x**3
+    elif (-8 <= x) and (x < 36):
+        return 2*x**4 + math.exp(x)
+    elif x >= 36:
+        return 66*x**2 - x**6
+    else : print("Error")
+print("Задача 1.2. Реализовать кусочно­линейную функцию")
+print ("f(-24) = {:.2e}".format(second(-24)))
+print ("f(25) = {:.2e}".format(second(25)))
+print()
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def third(n, m):
+    return 36*fu1(n) + 45*fu2(n, m)
+def fu1(n):
+    summ = 0
+    for i in range(1, n+1):
+        summ += i**5/78 - 21*i**3
+    return summ
+def fu2(n, m):
+    summ = 0
+    for i in range(1, n+1):
+        for j in range(1, m+1):
+            summ += 99*i**8 - 52*i + 13
+    return summ
+print("Задача 1.3. Реализовать итерационную функцию")
+print ("f(70, 95) = {:.2e}".format(third(70, 95)))
+print ("f(36, 27) = {:.2e}".format(third(36, 27)))
+print()
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+def fourth(n):
+    if n == 0:
+        return 3
+    return math.sin(float(fourth(n - 1))) + (1/72)*float((fourth(n-1)))**3
+print("Задача 1.4. Реализовать рекуррентную функцию")
+print ("f(8) = {:.2e}".format(fourth(8)))
+print ("f(9) = {:.2e}".format(fourth(9)))
